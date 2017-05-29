53b35c79d040759262bedcb935dc2b6194d56923
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-05-29 12:26:48 +0200

    Clear interval on disconnect; log messages; fixed #1

--- server.js
+++ server.js
@@ -13,13 +13,18 @@
 
     wsServer.on('request', request => {
         let connection = request.accept(null, request.origin);
-        console.log('Websocket connection established');
+        console.log('WebSocket connection established');
 
-        setInterval(() => {
+        let interval = setInterval(() => {
             let randomNumber = getRandomNumber();
             connection.send(randomNumber);
             console.log('[WS] ' + randomNumber);
         }, 1000);
+
+        connection.on('close', () => {
+            clearInterval(interval);
+            console.log('WebSocket connection closed');
+        });
     });
 })();
 
@@ -30,20 +35,27 @@
     
     let port = 4444;
     server.listen(port, () => {
-        console.log('socket.io server running on port', port);
+        console.log('socket.io server running on port', port, '\n\n');
     });
 
     const io = require('socket.io')(server);
 
-    setInterval(() => {
-        let randomNumber = getRandomNumber();
-        io.emit('message', randomNumber);
-        console.log('[IO] ' + randomNumber);
-    }, 1000)
-
     io.on('connection', socket => {
         console.log('socket.io connection established');
+
+        let interval = setInterval(() => {
+            let randomNumber = getRandomNumber();
+            socket.emit('message', randomNumber);
+            console.log('[IO] ' + randomNumber);
+        }, 1000);
+
+        socket.on('disconnect', () => {
+            clearInterval(interval);
+            console.log('socket.io connection closed');
+        });
     });
+
+
 })();
 
 
